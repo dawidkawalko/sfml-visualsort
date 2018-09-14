a3a13bfb4b1fe5a77f4d8022c515c389633114f7
Dawid Kawalko <dawid.kawalko@pezal.loc>
2018-09-14 10:43:44 +0200

    Add isRunning flag

---
+++ VisualSort/sort.cpp
@@ -0,0 +1,11 @@
+#include "sort.hpp"
+
+Sort::Sort()
+{
+	m_isRunning = false;
+}
+
+bool Sort::isRunning() const
+{
+	return m_isRunning;
+}
--- VisualSort/sort.hpp
+++ VisualSort/sort.hpp
@@ -7,8 +7,14 @@
 class Sort
 {
 public:
+	Sort();
+
 	// every sub-class has to implement this method
-	virtual void start(std::vector<Sortable>& array) const = 0;
+	virtual void start(std::vector<Sortable>& array) = 0;
+	bool isRunning() const;
+
+protected:
+	bool m_isRunning;
 };
 
 #endif // SORT_HPP
