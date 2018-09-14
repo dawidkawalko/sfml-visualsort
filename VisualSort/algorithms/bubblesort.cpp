bc6f7f11a75005d1720ecb0778d7da068e858b0f
Dawid Kawalko <dawid.kawalko@pezal.loc>
2018-09-14 10:49:01 +0200

    Implement the algorithm

--- VisualSort/algorithms/bubblesort.cpp
+++ VisualSort/algorithms/bubblesort.cpp
@@ -1,6 +1,26 @@
 #include "bubblesort.hpp"
 
-void BubbleSort::start(std::vector<Sortable>& array) const
+void BubbleSort::start(std::vector<Sortable>& array)
 {
+	m_isRunning = true;
 
+	for (unsigned i = 0; i < array.size(); i++)
+	{
+		bool hasSwapped = false;
+		for (unsigned j = 0; j < array.size() - i - 1; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				swap(array[j], array[j + 1]);
+				hasSwapped = true;
+			}
+		}
+
+		if (!hasSwapped)
+		{
+			break;
+		}
+	}
+
+	m_isRunning = false;
 }
