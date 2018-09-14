7597ab42059b990b473bafc3327440824bc6e66c
Dawid Kawalko <dawid.kawalko@pezal.loc>
2018-09-14 10:28:34 +0200

    Create Sortable class

---
+++ VisualSort/sortable.cpp
@@ -0,0 +1,19 @@
+#include "sortable.hpp"
+
+Sortable::Sortable(const unsigned value)
+{
+	m_value = value;
+	m_shape.setFillColor(sf::Color::White);
+}
+
+void Sortable::setSize(const float width, const float height)
+{
+	m_shape.setSize(sf::Vector2f(width, height));
+	m_shape.setOrigin(sf::Vector2f(width, height));
+}
+
+void Sortable::render(sf::RenderWindow& window, const unsigned position)
+{
+	m_shape.setPosition(position, window.getSize().y);
+	window.draw(m_shape);
+}
