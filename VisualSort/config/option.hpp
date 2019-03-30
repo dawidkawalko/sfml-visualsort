53e6f8d8b5521e2339010905fd3f3966d6f65161
dawidkawalko <dawid.kawalko.97@gmail.com>
2019-03-30 10:31:05 +0100

    Add missing comments

--- VisualSort/config/option.hpp
+++ VisualSort/config/option.hpp
@@ -6,9 +6,21 @@
 class Option
 {
 public:
+	/**
+	* Default constructor
+	*/
 	Option() : Option("") {}
+
+	/**
+	* Constructor, sets the value of this option
+	*
+	* @param value Option value
+	*/
 	explicit Option(const std::string& value) : m_value(value) {}
 
+	/**
+	* Returns the value as int
+	*/
 	operator int() const;
 
 private:
